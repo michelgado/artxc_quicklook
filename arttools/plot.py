6c9f23f47c8d860e06fb01ed47fae726ad320ba2
andrey <san@iki.rssi.ru>
2019-08-15 13:01:11 +0300

    I can plot images on the skygit diffgit diff

--- arttools/plot.py
+++ arttools/plot.py
@@ -3,26 +3,13 @@ from scipy.spatial.transform import Rotation, Slerp
 import numpy as np
 from astropy.visualization import MinMaxInterval, SqrtStretch, ImageNormalize, LogStretch
 from astropy.wcs import WCS
-from .orientation import extract_raw_gyro
+from .orientation import extract_raw_gyro, qort0, ART_det_QUAT
 from math import pi, cos, sin
 import copy
 
-qrot0 = Rotation([sin(-15.*pi/360.), 0., 0., cos(-15.*pi/360.)])
-
-ART_det_QUAT = {
-        28 : Rotation([-0.0110150413845477,     -0.0013329854433192,     -0.0010840551373762,      0.9999378564878738]),
-        22 : Rotation([-0.0097210046441978,     -0.0012050978602830,     -0.0010652583365244,      0.9999514563380221]),
-        23 : Rotation([-0.0109821945673236,     -0.0013205569544235,     -0.0010833616131844,      0.9999382350222591]), 
-        24 : Rotation([-0.0108749144342713,     -0.0012081051721620,     -0.0009784162714686,      0.9999396578891848]), 
-        25 : Rotation([-0.0083598094506972,     -0.0012398390463856,     -0.0011014394837848,      0.9999636809485386]), 
-        26 : Rotation([-0.0100908546351636,     -0.0012650094280487,     -0.0011698124374266,      0.9999476015985739]),
-        30 : Rotation([-0.0108764670901360,     -0.0012574641047721,     -0.0010592143554341,      0.9999394978260493])
-                }
-
-
 detwcs = WCS(naxis=2)
 detwcs.wcs.cdelt = np.array([45., 45.])/3600.
-detwcs.wcs.crval = [24.5, 24.5]
+detwcs.wcs.crpix = [24.5, 24.5]
 
 def get_rawxy_hist(data, phabotmin = 150, phabotmax = 300, mask = None):
     mask = (data["PHA_BOT"] > phabotmin) & (data["PHA_BOT"] < phabotmax)
@@ -38,17 +25,14 @@ def logstretch(img):
 def get_sky(urddata, URDN, attdata, xe, ye):
     qinit = qrot0*ART_det_QUAT[URDN]
     ra, dec, roll = extract_raw_gyro(attdata, qinit)
-    print(ra, dec, roll)
     ra, dec, roll = ra[0], dec[0], roll[0]
-    print(ra, dec, roll)
     locwcs = copy.copy(detwcs)
-    locwcs.wcs.crpix = np.array([ra, dec])*180./pi
-    print(locwcs.wcs)
+    locwcs.wcs.crval = np.array([ra, dec])*180./pi
     locwcs.wcs.pc = np.array([[cos(roll), -sin(roll)], [sin(roll), cos(roll)]])
 
-    ra, dec = locwcs.all_pix2world(np.array([urddata["RAW_X"], urddata["RAW_Y"]]).T, 0).T
-    plt.scatter(ra, dec)
-    plt.show()
+    x = np.tile(urddata["RAW_X"], 25) + np.repeat(np.tile(np.array([-0.4, -0.2, 0., 0.2, 0.4]), urddata.size), 5)
+    y = np.tile(urddata["RAW_Y"], 25) + np.tile(np.tile(np.array([-0.4, -0.2, 0., 0.2, 0.4]), urddata.size), 5)
+    ra, dec = locwcs.all_pix2world(np.array([x, y]).T, 0).T
     return np.histogram2d(ra, dec, [xe, ye])[0]
 
 
