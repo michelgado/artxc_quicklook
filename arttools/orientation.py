e0cd6f6e623216e8e165566765a361ecf7f008d1
andrey <san@iki.rssi.ru>
2019-08-18 12:00:55 +0300

    in plot module, get sky is splited on seeveral functions - it's appear convenient to use vectors to search in circle on sky

--- arttools/orientation.py
+++ arttools/orientation.py
@@ -2,12 +2,7 @@ from scipy.spatial.transform import Rotation, Slerp
 import numpy as np
 from math import pi, cos, sin
 
-#qrot0 = Rotation([sin(135.*pi/360.), 0., 0., cos(135.*pi/360.)])
-#qrot0 = Rotation([sin(45.*pi/360.), 0., 0., cos(45.*pi/360.)])
-qrot0 = Rotation([sin(-75*pi/360.), 0., 0., cos(-75*pi/360.)])
-#qrot0 = Rotation([sin(-255.*pi/360.), 0., 0., cos(-255.*pi/360.)])
-#qrot0 = Rotation([0., 0., 0., 1.])
-#qrot0 = Rotation([sin(195.*pi/360.), 0., 0., cos(195.*pi/360.)])
+qrot0 = Rotation([sin(15*pi/360.), 0., 0., cos(15*pi/360.)])
 
 ART_det_QUAT = {
         28 : Rotation([-0.0110150413845477,     -0.0013329854433192,     -0.0010840551373762,      0.9999378564878738]),
@@ -18,43 +13,14 @@ ART_det_QUAT = {
         26 : Rotation([-0.0100908546351636,     -0.0012650094280487,     -0.0011698124374266,      0.9999476015985739]),
         30 : Rotation([-0.0108764670901360,     -0.0012574641047721,     -0.0010592143554341,      0.9999394978260493])
                 }
-"""
-
-ART_det_QUAT = {
-        28 : Rotation([0.0110150413845477,     0.0013329854433192,     0.0010840551373762,      0.9999378564878738]),
-        22 : Rotation([0.0097210046441978,     0.0012050978602830,     0.0010652583365244,      0.9999514563380221]),
-        23 : Rotation([0.0109821945673236,     0.0013205569544235,     0.0010833616131844,      0.9999382350222591]), 
-        24 : Rotation([0.0108749144342713,     0.0012081051721620,     0.0009784162714686,      0.9999396578891848]), 
-        25 : Rotation([0.0083598094506972,     0.0012398390463856,     0.0011014394837848,      0.9999636809485386]), 
-        26 : Rotation([0.0100908546351636,     0.0012650094280487,     0.0011698124374266,      0.9999476015985739]),
-        30 : Rotation([0.0108764670901360,     0.0012574641047721,     0.0010592143554341,      0.9999394978260493])
-                }
-
-"""
-
-"""
-ART_det_QUAT = {
-        28 : Rotation([0., 0., 0., 1.]),
-        22 : Rotation([0., 0., 0., 1.]),
-        23 : Rotation([0., 0., 0., 1.]), 
-        24 : Rotation([0., 0., 0., 1.]), 
-        25 : Rotation([0., 0., 0., 1.]), 
-        26 : Rotation([0., 0., 0., 1.]),
-        30 : Rotation([0., 0., 0., 1.])
-                }
-
-"""
 
 def to_2pi_range(val): return val%(2.*pi)
-
 
 def get_gyro_quat(gyrodata):
     quat = Rotation(np.array([gyrodata["QORT_%d" % i] for i in [1,2,3,0]]).T)
     q0 = Rotation([0, 0, 0, 1]) #gyro axis initial rotattion in J2000 system
     qfin = q0*quat
     return qfin
-
-
 
 def extract_raw_gyro(gyrodata, qadd=Rotation([sin(-15.*pi/360.), 0., 0., cos(-15.*pi/360.)])):
     """
@@ -75,6 +41,6 @@ def extract_raw_gyro(gyrodata, qadd=Rotation([sin(-15.*pi/360.), 0., 0., cos(-15
 
     yzprojection = np.cross(opticaxis, [0., 0., 1.])
 
-    rollangle = np.arctan2(np.sum(yzprojection*qfin.apply([0, 0, 1]), axis=1), np.sum(yzprojection*qfin.apply([0, 1, 0]), axis=1))
+    rollangle = np.arctan2(np.sum(yzprojection*qfin.apply([0, 1, 0]), axis=1), np.sum(yzprojection*qfin.apply([0, 0, 1]), axis=1))
     return ra, dec, rollangle
 
--- arttools/plot.py
+++ arttools/plot.py
@@ -9,88 +9,57 @@ import copy
 
 dxya = np.arctan(0.595/2693.) #45./3600./180.*pi
 
-detwcs = WCS(naxis=2)
-detwcs.wcs.cdelt = np.array([45., 45.])/3600.
-detwcs.wcs.crpix = [24.5, 24.5]
-
-def get_rawxy_hist(data, phabotmin = 150, phabotmax = 300, mask = None):
+def get_rawxy_hist(data, phabotmin = 50, phabotmax = 300, mask = None):
     mask = (data["PHA_BOT"] > phabotmin) & (data["PHA_BOT"] < phabotmax)
     masked_data = data[mask]
     img = np.histogram2d(masked_data['RAW_X'], masked_data['RAW_Y'], [np.arange(49) - 0.5,]*2)[0]
     return img
 
-def logstretch(img):
-    norm = ImageNormalize(img, stretch=LogStretch())
-    plt.imshow(img, interpolation="nearest", origin="lower", norm=norm)
+def xy_to_vec(x, y):
+    """
+    assuming that the detector is located in the  YZ vizier plane and X is normal to it
+    we produce vectors, correspongin to the direction at which each particular pixel observe sky in the
+    vizier coordinate system
+    """
+    outvec = np.empty((x.size, 3), np.double)
+    outvec[:, 0] = 1.
+    outvec[:, 1] = np.tan((x - 23.5)*dxya)
+    outvec[:, 2] = np.tan((23.5 - y)*dxya)
+    return outvec 
 
 def urd_to_vec(urddata, subscale=1):
     sscale = (np.arange(subscale) - (subscale - 1)/2.)/subscale
-    print(sscale)
     x = np.repeat(urddata["RAW_X"], subscale*subscale) + \
             np.tile(np.tile(sscale, subscale), urddata.size)
     y = np.repeat(urddata["RAW_Y"], subscale*subscale) + \
             np.tile(np.repeat(sscale, subscale), urddata.size)
-    #return xy_to_vec(urddata["RAW_X"], urddata["RAW_Y"])
     return xy_to_vec(x, y)
 
-def xy_to_vec(x, y):
-    """
-    normshift 
-    """
-    outvec = np.empty((x.size, 3), np.double)
-    outvec[:, 0] = 1.
-    outvec[:, 2] = np.tan((x - 23.5)*dxya)
-    outvec[:, 1] = np.tan((y - 23.5)*dxya)
-    return outvec 
-
-
-def get_sky(urddata, URDN, attdata, xe, ye):
+def get_photons_vectors(urddata, URDN, attdata):
     qj2000 = Slerp(attdata["TIME"], get_gyro_quat(attdata))
-    qj2000 = qj2000(np.repeat(urddata["TIME"], 25))
-    #qj2000 = qj2000(urddata["TIME"])
-    qall = qj2000*ART_det_QUAT[URDN]*qrot0
-    #qall = qj2000*qrot0*ART_det_QUAT[URDN]
-    """
-    phvec = qall.apply([1., 0., 0.])
+    qj2000 = qj2000(urddata["TIME"])
+    qall = qj2000*qrot0*ART_det_QUAT[URDN]
+
+    photonvecs = urd_to_vec(urddata)
+    phvec = qall.apply(photonvecs)
+    return phvec
+
+def get_photons_sky_coord(urddata, URDN, attdata):
+    phvec = get_photons_vectors(urddata, URDN, attdata)
     dec = np.arctan(phvec[:,2]/np.sqrt(phvec[:,0]**2. + phvec[:,1]**2.))*180./pi
     ra = (np.arctan2(phvec[:,1], phvec[:,0])%(2.*pi))*180./pi
-    plt.plot(ra, dec, zorder=4)
+    return ra, dec
 
-    phvec = qall.apply([1., np.tan(-24*45./3600./180.*pi), np.tan(-24*45./3600./180.*pi)])
+def get_sky_image(urddata, URDN, attdata, xe, ye, subscale=1):
+    qj2000 = Slerp(attdata["TIME"], get_gyro_quat(attdata))
+    qj2000 = qj2000(np.repeat(urddata["TIME"], 25))  #subscale*subscale))
+    qall = qj2000*qrot0*ART_det_QUAT[URDN]
+
+    photonvecs = urd_to_vec(urddata, 5) #subscale)
+    phvec = qall.apply(photonvecs)
     dec = np.arctan(phvec[:,2]/np.sqrt(phvec[:,0]**2. + phvec[:,1]**2.))*180./pi
     ra = (np.arctan2(phvec[:,1], phvec[:,0])%(2.*pi))*180./pi
-    plt.plot(ra, dec, zorder=4)
-
-    phvec = qall.apply([1., np.tan(-24*45./3600./180.*pi), np.tan(24*45./3600./180.*pi)])
-    dec = np.arctan(phvec[:,2]/np.sqrt(phvec[:,0]**2. + phvec[:,1]**2.))*180./pi
-    ra = (np.arctan2(phvec[:,1], phvec[:,0])%(2.*pi))*180./pi
-    plt.plot(ra, dec, zorder=4)
-
-    phvec = qall.apply([1., np.tan(24*45./3600./180.*pi), np.tan(24*45./3600./180.*pi)])
-    dec = np.arctan(phvec[:,2]/np.sqrt(phvec[:,0]**2. + phvec[:,1]**2.))*180./pi
-    ra = (np.arctan2(phvec[:,1], phvec[:,0])%(2.*pi))*180./pi
-    plt.plot(ra, dec, zorder=4)
-    """
-
-
-    photonvecs = urd_to_vec(urddata, 5)
-    phvec = qall.apply(photonvecs) #[1., 0., 0.])
-    dec = np.arctan(phvec[:,2]/np.sqrt(phvec[:,0]**2. + phvec[:,1]**2.))*180./pi
-    ra = (np.arctan2(phvec[:,1], phvec[:,0])%(2.*pi))*180./pi
-    #plt.plot(ra, dec, zorder=4)
     
-    """
-    ra, dec, roll = extract_raw_gyro(attdata, qinit)
-    ra, dec, roll = ra[0], dec[0], roll[0]
-    locwcs = copy.copy(detwcs)
-    locwcs.wcs.crval = np.array([ra, dec])*180./pi
-    #locwcs.wcs.pc = np.array([[cos(roll), -sin(roll)], [sin(roll), cos(roll)]])
-    locwcs.wcs.pc = np.array([[cos(roll), sin(roll)], [-sin(roll), cos(roll)]])
-
-    x = np.tile(urddata["RAW_X"], 25) + np.repeat(np.tile(np.array([-0.4, -0.2, 0., 0.2, 0.4]), urddata.size), 5)
-    y = np.tile(urddata["RAW_Y"], 25) + np.tile(np.tile(np.array([-0.4, -0.2, 0., 0.2, 0.4]), urddata.size), 5)
-    ra, dec = locwcs.all_pix2world(np.array([x, y]).T, 0).T
-    """
     return np.histogram2d(dec, ra, [ye, xe])[0]
 
 
