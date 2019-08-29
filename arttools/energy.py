7b446703e978e57e1e8ef39265fe9436bd6af19f
andrey <san@iki.rssi.ru>
2019-08-29 11:40:06 +0300

    after several updates AS and MN spectra are much closer to each other, the differences are 1) double precision in MP subroutine 2) filter by rawx rawy in AS subroutine 3) first channel in MP subroutine in 0.1-0.2 keV 4) in AS subroutine temperature is taken from fits table column 5) in AS subroutine the livetime temporarely is computed like in MN subroutine (tottime - eventlist.size*770e-6)

--- arttools/energy.py
+++ arttools/energy.py
@@ -10,9 +10,10 @@ def PHA_to_PI(PHA, strip, Temp, caldb):
     return energy
 
 def random_uniform(PHA, strip, Temp, caldb):
-    e1 = PHA_to_PI(PHA - 0.5, strip, Temp, caldb)
-    e2 = PHA_to_PI(PHA + 0.5, strip, Temp, caldb)
+    e1 = PHA_to_PI(PHA, strip, Temp, caldb)
+    e2 = PHA_to_PI(PHA + 1., strip, Temp, caldb)
     return np.random.uniform(e1, e2)
+    #return PHA_to_PI(np.random.uniform(0., 1., PHA.shape) + PHA, strip, Temp, caldb)
 
 def filter_edge_strips(events):
     return np.all((events["RAW_X"] > 0, events["RAW_X"] < 47, events["RAW_Y"] > 0, events["RAW_Y"] < 47), axis=0)
@@ -52,11 +53,19 @@ def get_events_energy(eventlist, hkdata, caldb):
     """
     print("total events", eventlist.size)
 
-    #eventlist = eventlist[filter_edge_strips(eventlist)] 
+
+    m0 = filter_edge_strips(eventlist) 
+    eventlist = eventlist[m0]
 
     T = interp1d(hkdata["TIME"], hkdata["TD1"], 
             bounds_error=False, kind="linear", 
             fill_value=(hkdata["TD1"][0], hkdata["TD1"][-1]))(eventlist["TIME"])
+    print("intial T", T[0])
+    T = eventlist["TD"]
+    print("T in table", T[0])
+    print("temperature", np.mean(T))
+    print("first event", eventlist[0])
+    print("dtype", eventlist.dtype)
 
     botcal = caldb["BOT"].data
     topcal = caldb["TOP"].data
@@ -66,7 +75,8 @@ def get_events_energy(eventlist, hkdata, caldb):
     #maskb = np.ones(rawx.shape, np.bool)
     print(rawx.shape, maskb.size, maskb.sum())
     sigmab = botcal["fwhm_1"][rawx]*T + botcal["fwhm_0"][rawx]
-    PHAB = np.array([eventlist["PHA_BOT_SUB1"], eventlist["PHA_BOT"], eventlist["PHA_BOT_ADD1"]])
+    PHAB = np.array([eventlist["PHA_BOT_LEFT"], eventlist["PHA_BOT"], eventlist["PHA_BOT_RIGHT"]])
+    #energb = PHA_to_PI(PHAB, rawx, T, botcal) #random_uniform(PHAB, rawx, T, botcal) #PHA_to_PI(PHAB, rawx, T, botcal)
     energb = random_uniform(PHAB, rawx, T, botcal) #PHA_to_PI(PHAB, rawx, T, botcal)
     maskb = np.logical_and(maskb, energb > botcal["THRESHOLD"][rawx])
     print("tot size", maskb.shape[1])
@@ -76,7 +86,8 @@ def get_events_energy(eventlist, hkdata, caldb):
     #rawy = np.tile(eventlist["RAW_X"], (3, 1))
     #maskt = np.ones(rawy.shape, np.bool)
     sigmat = topcal["fwhm_1"][rawy]*T + topcal["fwhm_0"][rawy]
-    PHAT = np.array([eventlist["PHA_TOP_SUB1"], eventlist["PHA_TOP"], eventlist["PHA_TOP_ADD1"]])
+    PHAT = np.array([eventlist["PHA_TOP_LEFT"], eventlist["PHA_TOP"], eventlist["PHA_TOP_RIGHT"]])
+    #energt = PHA_to_PI(PHAT, rawy, T, topcal) #random_uniform(PHAT, rawy, T, topcal) #PHA_to_PI(PHAT, rawy, T, topcal)
     energt = random_uniform(PHAT, rawy, T, topcal) #PHA_to_PI(PHAT, rawy, T, topcal)
     maskt = np.logical_and(maskt, energt > topcal["THRESHOLD"][rawy])
     print("top dist", np.unique(maskt.sum(axis=0), return_counts=True))
@@ -88,11 +99,28 @@ def get_events_energy(eventlist, hkdata, caldb):
     #atleastone = np.logical_and(atleastone, masklf)
     print("drop by threshold", atleastone.size - atleastone.sum(), " from ", atleastone.size)
     energb, energt, sigmab, sigmat, rawx, rawy, maskb, maskt = (arr[:, atleastone] for arr in [energb, energt, sigmab, sigmat, rawx, rawy, maskb, maskt])
+    #m0[np.arange(m0.size)[m0][np.logical_not(atleastone)]] = False
     print("all that left", energt.shape)
-    xc = np.sum(energb*maskb*rawx, axis=0)/np.sum(maskb*energb, axis=0)
-    yc = np.sum(energt*maskt*rawy, axis=0)/np.sum(maskb*energb, axis=0)
-    centralzone = ((xc - 23.5)**2. + (yc - 23.5)**2.) < 25.**2.
+    #xc = np.sum(energb*maskb*rawx, axis=0)/np.sum(maskb*energb, axis=0)
+    #yc = np.sum(energt*maskt*rawy, axis=0)/np.sum(maskb*energb, axis=0)
+    
+    xc = rawx[1]
+    yc = rawy[1]
+    #centralzone = ((xc - 23.5)**2. + (yc - 23.5)**2.) < 25.**2.
+    centralzone = ((rawx[1] - 23.5)**2. + (rawy[1] - 23.5)**2.) < 25.**2.
+    #m0[np.arange(m0.size)[m0][np.logical_not(centralzone)]] = False
     print("drop by shadow", centralzone.size - centralzone.sum())
+    print("energy top", energt[:,0])
+    print("energy bot", energb[:,0])
+    print("coeffs top", topcal[rawy[:,0]])
+    print("coeffs bot", botcal[rawx[:,0]])
+    bfirst = botcal[rawx[:,0]]
+    print("coeffs T[0]", bfirst[0]["C0_0"] + bfirst[0]["C0_1"]*T[0], bfirst[0]["C1_0"] + bfirst[0]["C1_1"]*T[0], bfirst[0]["C2_0"] + bfirst[0]["C2_1"]*T[0])
+    print("coeffs T[0]", bfirst[1]["C0_0"] + bfirst[1]["C0_1"]*T[0], bfirst[1]["C1_0"] + bfirst[1]["C1_1"]*T[0], bfirst[1]["C2_0"] + bfirst[1]["C2_1"]*T[0])
+    print("coeffs T[0]", bfirst[2]["C0_0"] + bfirst[2]["C0_1"]*T[0], bfirst[2]["C1_0"] + bfirst[2]["C1_1"]*T[0], bfirst[2]["C2_0"] + bfirst[2]["C2_1"]*T[0])
+    print("temp", T)
+
+    xc, yc = xc[centralzone], yc[centralzone]
     energb, energt, sigmab, sigmat, rawx, rawy, maskb, maskt = (arr[:, centralzone] for arr in [energb, energt, sigmab, sigmat, rawx, rawy, maskb, maskt])
 
 
@@ -101,11 +129,14 @@ def get_events_energy(eventlist, hkdata, caldb):
     etop = np.sum(energt*maskt, axis=0)
     sigmatopsq = np.sum(sigmat**2.*maskt, axis=0)
     emean = (ebot*sigmatopsq + etop*sigmabotsq)/(sigmatopsq + sigmabotsq)
+    print("estimated coordinates", xc[0], yc[0])
+    print("before fine tuning", emean[0])
 
     """
     urd 28 additional correction
     """
     emean = -0.2504 + 1.0082*emean - 6.10E-5*emean**2.
-    return atleastone, emean
+    print("after fine tuning", emean[0])
+    return m0, emean, xc, yc
 
 
