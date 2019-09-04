248fe02b19703d6a5d67ccfea031c469e65c1932
andrey <san@iki.rssi.ru>
2019-09-04 11:03:06 +0300

    consider different methods to produce time series in gtis

--- arttools/time.py
+++ arttools/time.py
@@ -38,11 +38,24 @@ def hist_quat(quat):
 
 def hist_orientation(attdata, gti, v0=None):
     quatint = Slerp(attdata["TIME"], get_gyro_quat(attdata))
-    qval = quatint((attdata["TIME"][1:] + attdata["TIME"][:-1])/2.)
 
+    idx = np.searchsorted(attdata["TIME"], gti)
+    tnew = np.empty(np.sum(idx[:,1] - idx[:,0]) + 2*idx.shape[0])
+    cidx = np.empty(idx.shape[0] + 1, np.int)
+    cidx[1:] = np.cumsum(idx[:,1] - idx[:,0] + 2)
+    for i in range(idx.shape[0]):
+        tnew[cidx[i]+1: cidx[i+1]-1] = attdata["TIME"][idx[i,0]:idx[i:1]]
+        tnew[cidx[i]] = gti[i, 0]
+        tnew[cidx[i + 1] - 1] = gti[i, 1]
+    ts = [np.array([gti[i, 0] + list(attdata["TIME"][idx[i, 0]:idx[i:1]]) + gti[i, 1]]) for i in range(gti.shape[0])]
+    te = np.concatenate(ts)
+    dt = np.concatenate([t[1:] - t[:-1] for t in ts])
+    ts = np.concatenate([(t[1:] + t[:-1])/2. for t in ts])
+    qval = quatint(ts)
+
+    ra, dec, roll = quat_to_pol_and_roll(quatint(te))
     ra, dec, roll = extract_raw_gyro(attdata)
     vec = vec_to_pol(dec, ra)
-    dt = attdata["TIME"][1:] - attdata["TIME"][:-1]
     dalpha = np.arccos(vec[1:, :]*vec[:-1, :])
     droll = (roll[1:] - roll[:-1])
 
