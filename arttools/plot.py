ea8c189810632c2e76520c68f37efb7c001c4f5c
andrey <san@iki.rssi.ru>
2019-10-20 12:10:12 +0300

    no need for round, since it will be performed in histogram

--- arttools/plot.py
+++ arttools/plot.py
@@ -105,7 +105,7 @@ def make_vec_to_sky_hist_fun(vecs, effarea, locwcs, xsize, ysize):
     def hist_vec_to_sky(quat, weight):
         vec_icrs = quat.apply(vecs)
         r, d = vec_to_pol(vec_icrs)
-        x, y = np.round(locwcs.all_world2pix(np.array([r*180./pi, d*180./pi]).T, 1)).T
+        x, y = locwcs.all_world2pix(np.array([r*180./pi, d*180./pi]).T, 1).T
         locweight = weight*effarea
         return np.histogram2d(x, y, [np.arange(xsize + 1) + 0.5, np.arange(ysize + 1) + 0.5], weights=locweight)[0].T
     return hist_vec_to_sky
