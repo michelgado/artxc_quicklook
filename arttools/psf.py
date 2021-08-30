9b309aeb5140d0568633b732bb63a712c6f17529
Semena Andrey <san@iki.rssi.ru>
2021-08-30 12:50:21 +0300

    added function which provides normalized interpolation 'vignetting' functions for a single pixel

--- arttools/psf.py
+++ arttools/psf.py
@@ -1,14 +1,21 @@
-from .caldb import get_optical_axis_offset_by_device, get_inverse_psf_data, \
+from .caldb import get_optical_axis_offset_by_device, get_inverse_psf_data, get_arf, \
         get_inversed_psf_data_packed, get_inverse_psf_datacube_packed, get_ayut_inverse_psf_datacube_packed
+from .energy  import get_arf_energy_function
+from .spectr import get_filtered_crab_spectrum, Spec
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
+from scipy.integrate import quad
 
 def xy_to_opaxoffset(x, y, urdn):
     print("urdn:", urdn)
     x0, y0 = (23, 23) if urdn is None else get_optical_axis_offset_by_device(urdn)
     return np.round(x + 0.5 - x0).astype(np.int), np.round(y + 0.5 - y0).astype(np.int)
 
-def rawxy_to_opaxoffset(urddata, urdn):
+def rawxy_to_opaxoffset(rawx, rawy, urdn):
+    x, y = get_optical_axis_offset_by_device(urdn)
+    return np.round(rawx + 0.5 - x).astype(np.int), np.round(rawy + 0.5 - y).astype(np.int)
+
+def urddata_to_opaxoffset(urddata, urdn):
     x, y = get_optical_axis_offset_by_device(urdn)
     return np.round(urddata["RAW_X"] + 0.5 - x).astype(np.int), np.round(urddata["RAW_Y"] + 0.5 - y).astype(np.int)
 
@@ -51,7 +58,7 @@ def unpack_inverse_psf(i, j):
 
 ayutee = np.array([4., 6., 8., 10., 12., 16., 20., 24., 30.])
 
-def unpack_inverse_psf_ayut(i, j, e=None):
+def unpack_inverse_psf_ayut(i, j, e=None, data=None):
     """
     inverse psf is an integral of the product of psf and vignetting over ART-XC detectors pixels
     since this characteristics is a result of integral we cannot use differential approximation to extract
@@ -75,7 +82,7 @@ def unpack_inverse_psf_ayut(i, j, e=None):
     if ja > ia:
         ja, ia = ia, ja
     k = (ia + 1)*ia//2 + ja
-    data = get_ayut_inverse_psf_datacube_packed()
+    data = data if not data is None else get_ayut_inverse_psf_datacube_packed()
     data = data[k]
     if abs(j) > abs(i):
         data = np.transpose(data, axes=(0, 2, 1))
@@ -87,6 +94,36 @@ def unpack_inverse_psf_ayut(i, j, e=None):
         return data[np.searchsorted(ayutee, e) - 1]
     else:
         return data
+
+def unpack_inverse_psf_specweighted_ayut(imgfilter, cspec=None, app=None):
+    """
+    produces spectrum weights for ipsf channels
+    """
+    w = np.zeros(ayutee.size - 1, np.double)
+
+    if not cspec is None:
+
+        egloc, egaps = imgfilter["ENERGY"].make_tedges(ayutee)
+        ec = (egloc[1:] + egloc[:-1])[egaps]/2.
+        arf = get_arf_energy_function(get_arf())
+        cspec = np.array([quad(lambda e: arf(e)*cspec(e), elow, ehi)[0] for elow, ehi in zip(egloc[:-1][egaps], egloc[1:][egaps])]) #np.concatenate([cspec/cspec.sum()/np.diff(egrid), [0, ]])
+        cspec = cspec/cspec.sum()
+        np.add.at(w, np.searchsorted(ayutee, ec) - 1, cspec)
+    else:
+        rgrid, cspec = get_filtered_crab_spectrum(imgfilter, collapsegrades=True)
+        crabspec = Spec(rgrid["ENERGY"][:-1], rgrid["ENERGY"][1:], cspec)
+        egloc, egaps = imgfilter["ENERGY"].make_tedges(np.unique(np.concatenate([ayutee, rgrid["ENERGY"]])))
+        ec = (egloc[1:] + egloc[:-1])[egaps]/2.
+        cspec = crabspec.integrate_in_bins(np.array([egloc[:-1], egloc[1:]]).T[egaps])
+        cspec = cspec.sum()
+        np.add.at(w, np.searchsorted(ayutee, ec) - 1, cspec)
+    data = np.sum(get_ayut_inverse_psf_datacube_packed()*w[np.newaxis, :, np.newaxis, np.newaxis], axis=1)
+    print(data[0, 60, 60] + data[1, 60, 51]*4 + data[2, 51, 51]*4)
+    print(data.max())
+    data = data/(data[0, 60, 60] + data[1, 60, 51]*4 + data[2, 51, 51]*4)
+    def newfunc(i, j):
+        return unpack_inverse_psf_ayut(i, j, data=data)
+    return newfunc
 
 def get_ipsf_interpolation_func(app=6.*60):
     ipsf = get_inversed_psf_data_packed()
