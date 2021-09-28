90e49afd984b0b7abbb917bce598efea9f1df317
Semena Andrey <san@iki.rssi.ru>
2021-09-28 12:33:15 +0300

    restructuring vectros filtering procedure, opax offset filter and actual mask filtering in different places now

--- arttools/illumination.py
+++ arttools/illumination.py
@@ -1,7 +1,7 @@
 from .telescope import URDNS
 import numpy as np
 from .caldb import get_boresight_by_device, get_optical_axis_offset_by_device, get_illumination_mask, get_shadowmask_by_urd
-from ._det_spatial import offset_to_vec, raw_xy_to_vec, offset_to_qcorr
+from ._det_spatial import offset_to_vec, raw_xy_to_vec, rawxy_to_qcorr
 from .orientation import vec_to_pol, pol_to_vec, get_photons_vectors, make_align_quat
 from .interval import Intervals
 from .time import emptyGTI, GTI
@@ -9,12 +9,13 @@ from .telescope import URDNS
 from .psf import get_ipsf_interpolation_func, unpack_inverse_psf_specweighted_ayut, rawxy_to_opaxoffset
 from .mosaic import SkyImage, SkyImageMP
 from .atthist import make_small_steps_quats
+from copy import copy
 from math import pi, sin, cos
 from functools import reduce
 from astropy.wcs import WCS
 from multiprocessing.pool import ThreadPool
 from multiprocessing import cpu_count
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from threading import Thread
 
 
@@ -56,22 +57,22 @@ def get_events_in_illumination_mask(urdn, srcax, urdevts, attdata):
     return mask
 
 class IlluminationSource(object):
-    def __init__(self, ra, dec, wcs, offsets, imask):
+    def __init__(self, ra, dec, wcs, offsets, imask, app=300.):
         self.sourcevector = pol_to_vec(*np.deg2rad([ra, dec]))
         #wcs, offsets, imask = get_illumination_mask()
         self.wcs = wcs
         self.offsets = offsets
         self.imask = imask
-
+        self.x, self.y = np.mgrid[0:imask.shape[1]:1, 0:imask.shape[2]:1]
+        self.app = app
         self.cedges = None
         self.mask = None
         self.qalign = None
         self.sidx = None
 
-    def get_vectors_in_illumination_mask(self, quats, pvecs, opax, qalign=None):
+    def get_vectors_in_illumination_mask(self, quats, pvecs, opax):
         opaxvecs = quats.apply(opax)
-        if qalign is None:
-            qalign = make_align_quat(np.tile(self.sourcevector, (opaxvecs.shape[0], 1)), opaxvecs)
+        qalign = make_align_quat(np.tile(self.sourcevector, (opaxvecs.shape[0], 1)), opaxvecs)
 
         angles = np.arccos(np.sum(self.sourcevector*opaxvecs, axis=-1))*180./pi*3600.
         offedges = np.array([self.offsets["OPAXOFFL"], self.offsets["OPAXOFFH"]]).T
@@ -87,7 +88,6 @@ class IlluminationSource(object):
         cedges = np.searchsorted(angles[sidx], offedges)
         qloc = qalign[sidx]
 
-        xy = []
         for i, crval in enumerate(self.offsets["CRVAL"]):
             s, e = cedges[i]
             if e == s:
@@ -96,7 +96,16 @@ class IlluminationSource(object):
             self.wcs.wcs.crval[1] = crval/3600.
             w1 = WCS(self.wcs.to_header())
             xyl = (w1.all_world2pix(np.rad2deg(vec_to_pol(pvecsis)).T, 1) - 0.5).astype(int)
-            mask[sidx[s:e]] = self.imask[i][xyl[:, 1], xyl[:, 0]]
+            #imask = np.copy(self.imask[i])
+            y0, x0 = (w1.all_world2pix(np.array([[180., 0.],]), 1) - 0.5).astype(int)[0]
+            mask[sidx[s:e]] = self.imask[np.full(xyl.shape[0], i), xyl[:, 1], xyl[:, 0]] & ((xyl[:, 1] - x0)**2 + (xyl[:, 0] - y0)**2. > (self.app/w1.wcs.cdelt[0]/3600.)**2.)
+            #imask[(self.x - x0)**2 + (self.y - y0)**2. < (self.app/w1.wcs.cdelt[0]/3600.)**2.] = False
+            """
+            print(y0, x0, w1.all_world2pix(np.array([[180., 0.],]), 1))
+            plt.imshow(imask)
+            plt.show()
+            """
+            #mask[sidx[s:e]] = imask[xyl[:, 1], xyl[:, 0]]
         return mask
 
     def setup_for_quats(self, quats, opax):
@@ -112,12 +121,9 @@ class IlluminationSource(object):
     def mask_vecs_with_setup(self, pvecs, qinitrot=None, mpnum=MPNUM, opax=None):
         pool = ThreadPool(mpnum)
         qvecrot = self.qalign if qinitrot is None else self.qalign*qinitrot
-        #pvecsis = np.array(pool.map(qvecrot.apply, pvecs))[:, self.sidx, :]
-        #mask = np.zeros(pvecsis.shape[:2], np.bool)
         mask = np.zeros((pvecs.shape[0], len(qvecrot)), np.bool)
         qloc = qvecrot[self.sidx]
         print(len(qloc))
-        #print("check shapes", pvecsis.shape, self.imask.shape, mask.shape)
         iidx = np.arange(mask.shape[1])
         for i, crval in enumerate(self.offsets["CRVAL"]):
             s, e = self.cedges[i]
@@ -125,25 +131,12 @@ class IlluminationSource(object):
                 continue
             self.wcs.wcs.crval[1] = crval/3600.
             w1 = WCS(self.wcs.to_header())
-            #print(s, e)
             pvecsis = np.array(pool.map(qloc[s:e].apply, pvecs)).reshape((-1, 3))
-            #print("indexees", s, e)
-            #xyl = (w1.all_world2pix(np.rad2deg(vec_to_pol(pvecsis[:, s:e].reshape((-1, 3)))).T, 1) - 0.5).astype(int)
             xyl = (w1.all_world2pix(np.rad2deg(vec_to_pol(pvecsis)).T, 1) - 0.5).astype(int)
-            """
-            plt.imshow(self.imask[i])
-            plt.scatter(xyl[:, 0], xyl[:, 1], marker="x")
-            if not opax is None:
-                xyo = (w1.all_world2pix(np.rad2deg(vec_to_pol(qvecrot[s:e].apply(opax))).T, 1) - 0.5).astype(int)
-                plt.scatter(xyo[:, 0], xyo[:, 1], marker="+")
-            plt.show()
-            print(xyl[:, 0].min(), xyl[:, 0].max(), xyl[:, 1].min(), xyl[:, 1].max())
-            """
-            #mloc = mask[:, self.sidx][:, s:e].reshape(-1)
-            #mloc[:] = self.imask[i][xyl[:, 0], xyl[:, 1]]
-            #print(mloc.size, mloc.sum())
-            mask[:, self.sidx[s:e]] = self.imask[i, xyl[:, 1], xyl[:, 0]].reshape((-1, e - s))
-            #print("check mask", mask.sum())
+            imask = np.copy(self.imask[i])
+            y0, x0 = (w1.all_world2pix(np.array([[180., 0.],]), 1) - 0.5).astype(int)[0]
+            imask[(self.x - x0)**2 + (self.y - y0)**2. < (self.app/w1.wcs.cdelt[0]/3600.)**2.] = False
+            mask[:, self.sidx[s:e]] = imask[xyl[:, 1], xyl[:, 0]].reshape((-1, e - s))
         return mask
 
     def get_events_in_illumination_mask(self, urddata, urdn, attdata):
@@ -151,8 +144,9 @@ class IlluminationSource(object):
         opax = raw_xy_to_vec(*np.array(get_optical_axis_offset_by_device(urdn)).reshape((2, 1)))[0]
         gti = attloc.circ_gti(self.sourcevector, self.offsets["OPAXOFFH"][-1], opax)
         mask = gti.mask_external(urddata["TIME"])
-        pvecs = get_photons_vectors(urddata[mask], urdn, attdata)
-        mask[mask] = self.get_vectors_in_illumination_mask(attloc(urddata["TIME"][mask]), pvecs, opax)
+        if np.any(mask):
+            pvecs = get_photons_vectors(urddata[mask], urdn, attdata)
+            mask[mask] = self.get_vectors_in_illumination_mask(attloc(urddata["TIME"][mask]), pvecs, opax)
         return mask
 
 class IlluminationSources(object):
@@ -184,12 +178,12 @@ class IlluminationSources(object):
             mask = np.logical_or(mask, source.get_vectors_in_illumination_mask(quats, pvecs, opax, None if not qalignlist else qalignlist[i]))
         return mask
 
-    def get_illumination_expmap(self, wcs, attdata, urdgti, imgfilters, urdweights={}, dtcorr={}, **kwargs):
+    def get_illumination_expmap(self, wcs, attdata, urdgti, imgfilters, urdweights={}, dtcorr={}, mpnum=MPNUM, kind="stright", **kwargs):
         """
         note: deadtime correction is likely mandatory in the vicinity of illumination sources
         """
         vmap = get_ipsf_interpolation_func()
-        sky = SkyImageMP(wcs, vmap)
+        sky = SkyImageMP(wcs, vmap, mpnum=mpnum)
 
 
         for urdn, gti in urdgti.items():
@@ -199,6 +193,8 @@ class IlluminationSources(object):
             bti = self.get_illumination_bti(attdata, [urdn,])
             lgti = gti & bti
             print("filters and nonfiltered exposures", gti.exposure, lgti.exposure)
+            if lgti.exposure == 0:
+                continue
             ts, qval, dtq, locgti = make_small_steps_quats(attdata, gti=lgti, timecorrection=dtcorr.get(urdn, lambda x: np.ones(x.size)))
             dtq = dtq*urdweights.get(urdn, 1./7.)
             for source in self.sources:
@@ -206,32 +202,13 @@ class IlluminationSources(object):
 
             shmask = get_shadowmask_by_urd(urdn)
             xloc, yloc = self.x[shmask], self.y[shmask]
-            qcorr = offset_to_qcorr(xloc, yloc)
+            qcorr = rawxy_to_qcorr(xloc, yloc)
             vecs = raw_xy_to_vec(self.x[shmask], self.y[shmask])
             i, j = rawxy_to_opaxoffset(xloc, yloc, urdn)
 
-            mask = np.any([source.mask_vecs_with_setup(vecs, qval, opax=opax) for source in self.sources], axis=0)
-            #print("final mask parameters", mask.sum(), mask.size, mask.shape)
-            print("urdn %d process:" % urdn)
-            sky.interpolate_bunch([(qval[m]*q, dtq[m], ipsffunc(il, jl)) for m, q, il, jl in zip(mask, qcorr, i, j) if np.any(m)])
-
-            """
-            thread = Thread(target=lambda x: None, args=(None,))
-            thread.start()
-
-            for il, jl, q, v in zip(i, j, qcorr, vecs):
-                mask = np.any([source.mask_vecs_with_setup(qval.apply(v)) for source in self.sources], axis=0)
-                print(il, jl, mask.sum(), mask.size)
-                thread.join()
-                sky._set_core_values(ipsffunc(il, jl))
-                thread = Thread(target=sky.interpolate, args=(qval[mask]*q, dtq[mask]))
-                thread.start()
-                #sky.interpolate(qval[mask]*q, dtq[mask])
-            """
-            """
-                taskargs.append([qval[mask]*q, dtq[mask], ipsffunc(il, jl)])
-            sky.interpolate_bunch(taskargs)
-                """
+            mask = np.any([source.mask_vecs_with_setup(vecs, qval, mpnum=mpnum) for source in self.sources], axis=0)
+            print("urdn %d process (featuring %d workers):" % (urdn, mpnum))
+            sky.interpolate_bunch([(qval[m]*q, dtq[m], ipsffunc(il, jl)) for m, q, il, jl in zip(mask, qcorr, i, j) if np.any(m)], kind=kind)
 
         sky._accumulate_img()
         sky._clean_img()
