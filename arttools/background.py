updated sensitivity estimation; corrected error of the background normalization in the sensitivity estimation -- get_surface_background returns a proper accumulated background countrates if normalization is False, therefore it cannot be multiplied on other backgroud count rate. The sensitivity is now can be assessed with 2nd component of Taylor series decomposition

arttools/background.py
@@ -160,7 +160,7 @@ def get_background_surface_brigtnress(urdn, filters, fill_value=np.nan, normaliz
     #shmask = filters.apply(np.column_stack([x.ravel(), y.ravel()]).ravel().view([("RAW_X", np.int), ("RAW_Y", np.int)])).reshape(x.shape)
     shmask = filters.meshgrid(["RAW_Y", "RAW_X"], [np.arange(48), np.arange(48)])
     profile[~shmask] = fill_value
-    return profile/profile.sum() if normalize else profile
+    return profile/np.sum(profile[~np.isnan(profile)]) if normalize else profile
 
 def get_local_bkgrates(udata, bkglc):
     profile = get_background_surface_brigtnress(udata.urdn, udata.filters, 0., True)

arttools/illumination.py
@@ -8,7 +8,8 @@ from .filters import Intervals, IndependentFilters
 from .time import emptyGTI, GTI
 from .aux import DistributedObj
 from .vector import vec_to_pol, pol_to_vec, normalize
-from .vignetting import DetectorVignetting, DEFAULVIGNIFUN
+from .vignetting import DetectorVignetting, DEFAULVIGNIFUN, sensitivity_second_order
+from .background import get_background_surface_brigtnress
 from .telescope import URDNS
 from .psf import get_ipsf_interpolation_func, unpack_inverse_psf_specweighted_ayut, rawxy_to_opaxoffset, \
     unpack_inverse_psf_with_weights, get_pix_overall_countrate_constbkg_ayut, get_urddata_opaxofset_map, naive_bispline_interpolation, \
@@ -313,10 +314,21 @@ class IlluminationSources(object):
         if not filters is None:
             self.set_urdn_data_filters(filters)
 
-    def set_urdn_data_filters(self, filters):
+    def set_urdn_data_filters(self, filters, scales=None, brates=None, vfun=None):
         self.filters = filters
         self.detmap = {urdn: DetectorVignetting(unpack_inverse_psf_specweighted_ayut(f.filters)) for urdn, f in filters.items()}
         self.shmasks = {urdn: f.filters.meshgrid(["RAW_Y", "RAW_X"], [np.arange(48), np.arange(48)]) for urdn, f in filters.items()}
+        if not brates is None:
+            for urdn in self.detmap:
+                brate = get_background_surface_brigtnress(urdn, filters[urdn], normalize=True)*brates[urdn]
+                self.detmap[urdn].set_bkgratemap(brate)
+        if not scales is None:
+            for urdn in self.detmap:
+                self.detmap[urdn].set_vignscale(scales[urdn])
+        if not vfun is None:
+            for urdn in self.detmap:
+                self.detmap[urdn].set_vignetting_functions(vfun)
+
 
     def check_pixel_in_illumination(self, urdn, xof, yof, qloc, pixvec=None, mask=None):
         if mask is None:
@@ -421,8 +433,8 @@ class WCSSkyWithIllumination(WCSSky, IlluminationSources): #, IlluminationSource
         #WCSSky.__init__(self, isources, *args, filters=filters, **kwargs)
 
     @DistributedObj.for_each_process
-    def update_filters(self, filters):
-        self.set_urdn_data_filters(filters)
+    def update_filters(self, filters, **kwargs):
+        self.set_urdn_data_filters(filters, **kwargs)
 
     @DistributedObj.for_each_process
     def set_urdn(self, urdn):
@@ -462,6 +474,26 @@ class WCSSkyWithIllumination(WCSSky, IlluminationSources): #, IlluminationSource
 
         self.accumulate_img()
         return np.copy(self.img)
+
+    def get_theta_sq_component(self, attdata, urdfilters, brates, urdweights={}):
+        urdgtis = {urdn: f.filters["TIME"] for urdn, f in urdfilters.items()}
+        self.update_filters({urdn: f.filters for urdn, f in urdfilters.items()}, brates=brates, scales={urdn: urdweights.get(urdn, 1/7.) for urdn in urdfilters}, vfun=sensitivity_second_order)
+
+        for urdn in urdgtis:
+            self.set_urdn(urdn)
+            gti = urdgtis[urdn]
+            if gti.exposure == 0:
+                print("urd %d has no individual gti, continue" % urdn)
+                continue
+            print("urd %d, exposure %.1f, progress:" % (urdn, gti.exposure))
+            exptime, qval, locgti = hist_orientation_for_attdata(attdata.for_urdn(urdn), gti, \
+                                                                wcs=self.locwcs)
+            for _ in tqdm.tqdm(self.interpolate_vmap_for_qval(zip(qval, exptime)), total=exptime.size):
+                pass
+
+        self.accumulate_img()
+        return np.copy(self.img)
+
 
 
 

arttools/sensitivity.py
@@ -14,7 +14,7 @@ from .planwcs import ConvexHullonSphere, convexhull_to_wcs
 from .psf import xy_to_opaxoffset, unpack_inverse_psf_ayut, unpack_inverse_psf_ayut, unpack_inverse_psf_with_weights, get_ipsf_interpolation_func, naive_bispline_interpolation, photbkg_pix_coeff
 from .mosaic2 import SkyImage
 from .spectr import get_filtered_crab_spectrum, Spec
-from .vignetting import get_blank_vignetting_interpolation_func
+from .vignetting import get_blank_vignetting_interpolation_func, sensitivity_second_order, make_vignetting_for_urdn
 from .illumination import DataDistributer
 from scipy.optimize import minimize, root
 
@@ -31,6 +31,128 @@ import pickle
 
 MPNUM = cpu_count()
 
+"""
+let assume, that rate estimate is correct, then mean expected Theta is
+\sum ln((rs_i + b_i)/b_i) (rs_i + b_i) dt_i
+if we produce Teylor sequence from the logarithm
+
+ln(1 + rs/b)*(rs + b) = \sum_k (-1)^{k+1)/r (rs/b)^k (rs + b) = \
+    \ sum_k (-1)^{k+1)/k (rs/b)^{k + 1) b +  \sum_k (-1)^{k + 1} (rs/b)^k b = \
+    rs + \sum_k (-1)^{k + 1}(1/k - 1/(k + 1)) (rs/b)^{k + 1} b =
+    rs + \sum_k (-1)^{k + 1}/(k*(k + 1)) (rs/b)^{k + 1} b
+
+
+therefore, the logarithm can be presented as a Teylor sequenc
+
+"""
+def make_theta_2nd_order_overall_profile(imgfilters, brates, urdweights={}, subgrid=10, **kwargs):
+    """
+    produces combined effective area of seven detector as projected on sky
+
+    --------------
+    Parameters:
+        same as for make_vignetting_for_urdn, with additional arguments of urdweights - which weight vignetting map of each urd
+
+    returns:
+        scipy.interpolate.RegularGridInterpolator provideds projection of the effective area of seven detectors on sky, depending on offset from the "telescope axis" defined
+        by mean quaternion stored in CALDB, offsets can be coverted to vectors with arttools._det_spatial.offset_to_vec which assumes focal length arttools._det_spatial.F
+    """
+    if subgrid < 1:
+        print("ahtung! subgrid defines splines of the translation of multiple vigneting file into one map")
+        print("set subgrid to 2")
+        subgrid = 2
+    #x, y = np.meshgrid(np.linspace(-24., 24., 48*subgrid), np.np.linspace(-24., 24., 48*subgrid))
+    xmin, xmax = -40.*DL, 40.*DL
+    ymin, ymax = -40.*DL, 40.*DL
+
+    vecs = offset_to_vec(np.array([xmin, xmax, xmax, xmin]),
+                         np.array([ymin, ymin, ymax, ymax]))
+
+    vmaps = {}
+    for urdn in URDNS:
+        quat = get_boresight_by_device(urdn)
+        xlim, ylim = vec_to_offset(quat.apply(vecs))
+        xmin, xmax = min(xmin, xlim.min()), max(xmax, xlim.max())
+        ymin, ymax = min(ymin, ylim.min()), max(ymax, ylim.max())
+
+    dd = DL/subgrid
+    dx = dd - (xmax - xmin)%dd
+    xmin, xmax = xmin - dx/2., xmax + dx
+    dy = dd - (ymax - ymin)%dd
+    ymin, ymax = ymin - dy/2., ymax + dy
+
+    x, y = np.mgrid[xmin:xmax:dd, ymin:ymax:dd]
+    shape = x.shape
+    newvmap = np.zeros(shape, np.double)
+    vecs = offset_to_vec(np.ravel(x), np.ravel(y))
+
+    for urdn in URDNS:
+        vmap = make_vignetting_for_urdn(urdn, imgfilters[urdn].filters, brate=brates[urdn], scale=urdweights.get(urdn, 1./7.), vfun=sensitivity_second_order)
+        quat = get_boresight_by_device(urdn)
+        newvmap += vmap(vec_to_offset_pairs(quat.apply(vecs, inverse=True))).reshape(shape)
+
+    mask = newvmap > 0.
+    mx = mask.any(axis=1)
+    my = mask.any(axis=0)
+    mask = mx[:, np.newaxis] & my[np.newaxis, :]
+    vmapnew = np.copy(newvmap[mx, :][:, my])
+    vmapnew[np.isnan(vmapnew)] = 0.0
+
+    vmap = RegularGridInterpolator((x[:, 0][mx], y[0][my]), vmapnew, bounds_error=False, fill_value=0)
+    return vmap
+
+
+def get_theta_teylor_component(sky, attdata, imgfilters, brates, kind="direct", urdweights={}, **kwargs):
+    """
+    produce exposure map on the provided wcs area, with provided GTI and attitude data
+
+    There are two hidden nonobvious properties of the input data expected:
+    1) gti is expected to be a dict with key is urd number
+        and value is elevant for this urd gti in the form of Nx2 numpy array
+    2) wcs is expected to be astropy.wcs.WCS class,
+        crpix is expected to be exactly the central pixel of the image
+    """
+    urdgtis = {urdn: f.filters["TIME"] for urdn, f in imgfilters.items()}
+
+
+    if kind not in ["direct", "convolve"]:
+        raise ValueError("only  convolve and direct option for exposure mosiac is available")
+
+
+    overall_gti = emptyGTI
+
+    dtcc = {urdn: 1. for urdn in URDNS}
+    overall_gti = reduce(lambda a, b: a & b, [urdgtis.get(URDN, emptyGTI) for URDN in URDNS])
+
+    print("overal exposure", overall_gti.exposure)
+
+    if overall_gti.exposure > 0:
+        exptime, qval, locgti = hist_orientation_for_attdata(attdata, overall_gti, wcs=None if not hasattr(sky, "locwcs") else sky.locwcs)
+        vmap = make_theta_2nd_order_overall_profile(imgfilters, brates=brates, urdweights=urdweights, **kwargs)
+        sky.set_vmap(vmap)
+        print("exptime sum", exptime.sum())
+        print("produce overall urds expmap")
+        if kind == "direct":
+            sky.direct_convolve(qval, exptime)
+        elif kind == "convolve":
+            sky.fft_convolve(qval, exptime)
+
+    for urdn in urdgtis:
+        gti = urdgtis[urdn] & ~overall_gti
+        if gti.exposure == 0:
+            print("urd %d has no individual gti, continue" % urdn)
+            continue
+        print("urd %d, exposure %.1f, progress:" % (urdn, gti.exposure))
+        exptime, qval, locgti = hist_orientation_for_attdata(attdata.for_urdn(urdn), gti, \
+                                                             wcs=None if not hasattr(sky, "locwcs") else sky.locwcs)
+        vmap = make_vignetting_for_urdn(urdn, imgfilters[urdn].filters, brate=brates[urdn], scale=urdweights.get(urdn, 1/7.), vfun=sensitivity_second_order, **kwargs)
+        sky.set_vmap(vmap)
+        if kind == "direct":
+            sky.direct_convolve(qval, exptime)
+        elif kind == "convolve":
+            sky.fft_convolve(qval, exptime)
+        print(" done!")
+    return np.copy(sky.img)
 
 
 def get_sb_distribution(ax, att, filters, urdbkg, wcs=None, photbkgrate=0., urdweights={}, dtcorr={}, illum_filters=None, cspec=None):